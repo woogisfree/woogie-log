ec97851c4c02703ef6a21fa67caf1b74dcab288a
jin <woogisfree@gmail.com>
2023-10-12 09:26:56 +0900

    🎉 Initial Commit

---
+++ src/main/java/com/example/woogisfree/WoogisfreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.woogisfree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoogisfreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoogisfreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/woogisfree/WoogisfreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.woogisfree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoogisfreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
