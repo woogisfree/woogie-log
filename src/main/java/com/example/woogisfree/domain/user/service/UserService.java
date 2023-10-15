c12aa56417220652afaee6d7560fa74361e0f080
jin <woogisfree@gmail.com>
2023-10-15 15:50:39 +0900

    (Feat) Spring Security 를 이용한 회원가입, 로그인 구현 (+Thymeleaf)

---
+++ src/main/java/com/example/woogisfree/domain/user/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.woogisfree.domain.user.service;
+
+import com.example.woogisfree.domain.user.dto.AddUserRequest;
+import com.example.woogisfree.domain.user.entity.User;
+import com.example.woogisfree.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public Long save(AddUserRequest request) {
+        return userRepository.save(User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword())).build()).getId();
+    }
+}
