b06fe42359a2a3954e62cb1e504abbd3b1be3f3d
woogie <woogisfree@gmail.com>
2023-10-15 19:49:23 +0900

    (Feat) RefreshToken domain, repository 추가

--- src/main/java/com/example/woogisfree/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/example/woogisfree/domain/article/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.example.woogisfree.domain.article.repository;
 
 import com.example.woogisfree.domain.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
