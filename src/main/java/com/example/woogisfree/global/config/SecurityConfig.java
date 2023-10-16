e1638fe958912a87e3248e1ad00615be61812290
woogie <woogisfree@gmail.com>
2023-10-16 22:48:40 +0900

    (Refactor) change the SecurityConfig source code lambda dsl

--- src/main/java/com/example/woogisfree/global/config/SecurityConfig.java
+++ src/main/java/com/example/woogisfree/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -14,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebSecurityConfig {
+public class SecurityConfig {
 
     private final UserDetailsService userService;
 
@@ -28,20 +29,17 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests()
-                .requestMatchers("/login", "/signup", "/user").permitAll()
-                .anyRequest().authenticated()
-                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/articles/**").permitAll()
+                        .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
                         .defaultSuccessUrl("/articles")
-                )
+                        .permitAll())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login")
-                        .invalidateHttpSession(true)
-                )
-                .csrf().disable()
-                .build();
+                        .invalidateHttpSession(true))
+                .rememberMe(Customizer.withDefaults()).build();
     }
 
     // 인증 관리자 관련 설정
