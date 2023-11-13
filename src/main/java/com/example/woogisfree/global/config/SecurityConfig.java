3a56cb6c1359e3dea4daf0a892575999dc8635db
woogie <woogisfree@gmail.com>
2023-11-14 01:08:51 +0900

    (feat) Add Spring Security except login page, html, js files

--- src/main/java/com/example/woogisfree/global/config/SecurityConfig.java
+++ src/main/java/com/example/woogisfree/global/config/SecurityConfig.java
@@ -1,67 +1,30 @@
-//package com.example.woogisfree.global.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//    private static final String[] PERMIT_URL_ARRAY = {
-//            //javascript
-//            "/static/**",
-//            "/templates/**",
-//            //swagger
-//            "/v3/api-docs/**",
-//            "/swagger-ui/**"
-//    };
-//
-//    @Bean
-//    public WebSecurityCustomizer configure() {
-//        return (web -> web.ignoring()
-//                .requestMatchers(toString())
-//                .requestMatchers(PERMIT_URL_ARRAY));
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .authorizeHttpRequests(authorize -> authorize
-//                        .requestMatchers("/login", "/signup", "/user").permitAll()
-//                        .requestMatchers("/api/articles/**").hasRole("ADMIN")
-//                        .requestMatchers("/articles/**").hasRole("ADMIN")
-//                        .anyRequest().authenticated())
-//                .formLogin(formLogin -> formLogin
-//                        .loginPage("/login")
-//                        .successForwardUrl("/articles")
-//                        .permitAll())
-//                .logout(logout -> logout
-//                        .logoutSuccessUrl("/login")
-//                        .invalidateHttpSession(true))
-//                .rememberMe(Customizer.withDefaults()).build();
-//    }
-//
-//    // 인증 관리자 관련 설정
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//            HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) throws Exception {
-//        return http.getSharedObject(AuthenticationManagerBuilder.class)
-//                .userDetailsService(userDetailService)
-//                .passwordEncoder(bCryptPasswordEncoder)
-//                .and()
-//                .build();
-//    }
-//
-//    @Bean
-//    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
-//
+package com.example.woogisfree.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    private static final String[] PERMIT_URL_ARRAY = {
+            "/login",
+            "/static/**",
+            "/templates/**",
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PERMIT_URL_ARRAY).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .permitAll()
+                ).build();
+    }
+}
