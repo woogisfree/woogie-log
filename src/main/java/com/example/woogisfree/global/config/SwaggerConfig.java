123338971268373bdde7616a67dc9b48e7f19c4b
woogie <woogisfree@gmail.com>
2024-03-23 23:11:07 +0900

    feat: Swagger 에 Jwt 인증 추가

--- src/main/java/com/example/woogisfree/global/config/SwaggerConfig.java
+++ src/main/java/com/example/woogisfree/global/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.woogisfree.global.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +14,14 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(apiInfo())
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
     public Info apiInfo() {
