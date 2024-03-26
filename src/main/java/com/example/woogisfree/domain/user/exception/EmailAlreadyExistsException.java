73d5a0079605153d259f31d0602fdd03f3f492e9
woogie <woogisfree@gmail.com>
2024-03-26 10:21:51 +0900

    fix: custom Exception 수정

--- src/main/java/com/example/woogisfree/domain/user/exception/EmailAlreadyExistsException.java
+++ src/main/java/com/example/woogisfree/domain/user/exception/EmailAlreadyExistsException.java
@@ -1,7 +1,24 @@
 package com.example.woogisfree.domain.user.exception;
 
 public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException() {
+        super();
+    }
+
     public EmailAlreadyExistsException(String message) {
         super(message);
+    }
+
+    public EmailAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EmailAlreadyExistsException(Throwable cause) {
+        super(cause);
+    }
+
+    protected EmailAlreadyExistsException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/com/example/woogisfree/domain/user/exception/PasswordMismatchException.java
+++ src/main/java/com/example/woogisfree/domain/user/exception/PasswordMismatchException.java
@@ -1,7 +1,24 @@
 package com.example.woogisfree.domain.user.exception;
 
 public class PasswordMismatchException extends RuntimeException {
+
+    public PasswordMismatchException() {
+        super();
+    }
+
     public PasswordMismatchException(String message) {
         super(message);
+    }
+
+    public PasswordMismatchException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PasswordMismatchException(Throwable cause) {
+        super(cause);
+    }
+
+    protected PasswordMismatchException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
