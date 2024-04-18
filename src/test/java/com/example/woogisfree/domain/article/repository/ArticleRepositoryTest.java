8c182d7418954265618ebc77bc8aab6fcc32dfd8
woogie <woogisfree@gmail.com>
2024-04-18 20:26:49 +0900

    fix: Article Repository test 수정
    Test Code 의 실행 순서에 의존하지 않도록 수정

--- src/test/java/com/example/woogisfree/domain/article/repository/ArticleRepositoryTest.java
+++ src/test/java/com/example/woogisfree/domain/article/repository/ArticleRepositoryTest.java
@@ -6,21 +6,23 @@ import com.example.woogisfree.domain.comment.repository.CommentRepository;
 import com.example.woogisfree.domain.user.entity.ApplicationUser;
 import com.example.woogisfree.domain.user.entity.UserRole;
 import com.example.woogisfree.domain.user.repository.UserRepository;
-import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@DataJpaTest(properties = {
-        "spring.datasource.url=jdbc:h2:mem:testdb",
-        "spring.jpa.hibernate.ddl-auto=create-drop"
-}, showSql = false)
+@DataJpaTest
+        (properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.jpa.hibernate.ddl-auto=create-drop"
+        }, showSql = false)
 class ArticleRepositoryTest {
 
     @Autowired
@@ -34,50 +36,36 @@ class ArticleRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        ApplicationUser userA = new ApplicationUser("firstnameA", "lastnameA", "usernameA", "email@emailA", "passwordA", UserRole.USER);
-        ApplicationUser userB = new ApplicationUser("firstnameB", "lastnameB", "usernameB", "email@emailB", "passwordB", UserRole.USER);
-        userRepository.save(userA);
-        userRepository.save(userB);
+        ApplicationUser user = new ApplicationUser("firstnameA", "lastnameA", "usernameA", "email@emailA", "passwordA", UserRole.USER);
+        userRepository.save(user);
 
-        Article article1 = new Article("title1", "content1", userA);
-        Article article2 = new Article("title2", "content2", userB);
+        Article article = new Article("title1", "content1", user);
 
         // userA, article1
-        Comment comment1 = new Comment("content1", userA, article1);
-        Comment comment2 = new Comment("content2", userA, article1);
-        Comment comment3 = new Comment("content3", userA, article1);
-        Comment comment4 = new Comment("content4", userA, article1);
-
-        // userB, article2
-        Comment comment5 = new Comment("content5", userB, article2);
-        Comment comment6 = new Comment("content6", userB, article2);
+        Comment comment1 = new Comment("content1", user, article);
+        Comment comment2 = new Comment("content2", user, article);
+        Comment comment3 = new Comment("content3", user, article);
+        Comment comment4 = new Comment("content4", user, article);
 
         commentRepository.save(comment1);
         commentRepository.save(comment2);
         commentRepository.save(comment3);
         commentRepository.save(comment4);
-        commentRepository.save(comment5);
-        commentRepository.save(comment6);
 
-        article1.getCommentList().add(comment1);
-        article1.getCommentList().add(comment2);
-        article1.getCommentList().add(comment3);
-        article1.getCommentList().add(comment4);
+        article.getCommentList().add(comment1);
+        article.getCommentList().add(comment2);
+        article.getCommentList().add(comment3);
+        article.getCommentList().add(comment4);
 
-        article2.getCommentList().add(comment5);
-        article2.getCommentList().add(comment6);
-
-        articleRepository.save(article1);
-        articleRepository.save(article2);
+        articleRepository.save(article);
     }
 
     @Test
-    @Transactional
+    @WithMockUser(username = "user", roles = "USER")
     void shouldFindAllArticlesWithComments() {
         List<Article> articles = articleRepository.findAll();
-        assertEquals(2, articles.size());
+        assertEquals(1, articles.size());
         assertTrue(articles.stream().anyMatch(article -> article.getTitle().equals("title1")));
-        assertTrue(articles.stream().anyMatch(article -> article.getTitle().equals("title2")));
 
         for (Article article : articles) {
             if (article.getTitle().equals("title1")) {
@@ -89,7 +77,7 @@ class ArticleRepositoryTest {
     }
 
     @Test
-    @Transactional
+    @WithMockUser(username = "user", roles = "USER")
     void shouldFindArticleByIdWithComments() {
         Long firstArticleId = articleRepository.findAll().get(0).getId();
         articleRepository.findAllById(firstArticleId).ifPresent(article -> {
