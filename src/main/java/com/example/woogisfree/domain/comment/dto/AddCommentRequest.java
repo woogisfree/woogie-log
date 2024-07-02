16d357dbbe0a9c664a1ee2a5b13ee16ceacc0701
woogie <woogisfree@gmail.com>
2024-07-03 00:29:51 +0900

    fix: 댓글 등록 버튼 동작 실패 수정

--- src/main/java/com/example/woogisfree/domain/comment/dto/AddCommentRequest.java
+++ src/main/java/com/example/woogisfree/domain/comment/dto/AddCommentRequest.java
@@ -2,8 +2,10 @@ package com.example.woogisfree.domain.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
